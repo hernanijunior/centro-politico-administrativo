9735b811ee07322178e568e3aee92a24f2a89904
Hernani <hernanijunior>
2025-08-22 17:54:17 -0400

    Primeiro upload do projeto

---
+++ data.js
@@ -0,0 +1,248 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama-1",
+      "name": "panorama 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.0702704316581162,
+        "pitch": 0.014583663534931901,
+        "fov": 1.2733156773635639
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.020217700272485928,
+          "pitch": 0.032215442639500935,
+          "rotation": 0,
+          "target": "1-panorama-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5121699871116334,
+          "pitch": -0.04242935771193501,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">SEFAZ - MT</font></font></font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">A Secretaria de Estado de Fazenda de Mato Grosso (SEFAZ MT) desempenha um papel crucial na administração pública do estado, sendo a principal responsável pela gestão das políticas fiscal, tributária, financeira e contábil. Sua atuação vai além da simples arrecadação de impostos, impactando diretamente no financiamento das políticas públicas e no desenvolvimento socioeconômico de Mato Grosso.</font></font></font></font>"
+        }
+      ]
+    },
+    {
+      "id": "1-panorama-2",
+      "name": "panorama 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1257652133804896,
+          "pitch": 0.016455511098842024,
+          "rotation": 0,
+          "target": "0-panorama-1"
+        },
+        {
+          "yaw": -0.023862719266489307,
+          "pitch": 0.059385633034612084,
+          "rotation": 0,
+          "target": "2-panorama-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5669790860899795,
+          "pitch": -0.004972891787463851,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Fórum Trabalhista de Cuiabá</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">È o principal órgão para a resolução de conflitos individuais e coletivos entre empregados e empregadores na sua área de jurisdição.</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "2-panorama-3",
+      "name": "panorama 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7624544116573944,
+          "pitch": 0.03788506906688127,
+          "rotation": 0,
+          "target": "1-panorama-2"
+        },
+        {
+          "yaw": -0.4296407858656668,
+          "pitch": 0.05646066322208476,
+          "rotation": 0,
+          "target": "3-panorama-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.1564783759213224,
+          "pitch": -0.029021428087315115,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">FAMATO</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">A Federação da Agricultura e Pecuária do Estado de Mato Grosso (Famato) é a principal entidade de representação dos produtores rurais mato-grossenses. Fundada em 1965, a Famato atua como a voz política e institucional do setor agropecuário, defendendo seus interesses e promovendo o desenvolvimento sustentável da atividade no estado que é uma potência do agronegócio brasileiro.</font></font></font></font>"
+        }
+      ]
+    },
+    {
+      "id": "3-panorama-4",
+      "name": "panorama 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0541737671110383,
+          "pitch": 0.029203825297901886,
+          "rotation": 0,
+          "target": "2-panorama-3"
+        },
+        {
+          "yaw": 0.1100051873446457,
+          "pitch": 0.035330123155894455,
+          "rotation": 0,
+          "target": "4-panorama-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.021737775974774,
+          "pitch": -0.13109513185647437,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">ABCZ</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">A Associação Brasileira dos Criadores de Zebu (ABCZ) é a principal entidade da pecuária zebuína no Brasil e a maior organização do setor no mundo. a ABCZ tem como missão fundamental promover o aumento sustentável da produção mundial de carne e leite por meio do registro genealógico, melhoramento genético e promoção das raças zebuínas.</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "4-panorama-5",
+      "name": "panorama 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0818605913853485,
+          "pitch": 0.004049526017688265,
+          "rotation": 0,
+          "target": "3-panorama-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0126857705017756,
+          "pitch": 0.13607694577756035,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">SEDUC - MT</font></font></font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">A Secretaria de Estado de Educação de Mato Grosso (Seduc-MT) é o órgão do Poder Executivo estadual responsável pela formulação, implementação e gestão da política educacional em todo o estado. Sua principal missão é garantir o acesso, a permanência e a qualidade da educação básica para todos os mato-grossenses, em conformidade com as diretrizes e bases da educação nacional.</font></font></font></font>"
+        },
+        {
+          "yaw": 0.8012436405036087,
+          "pitch": -0.11358506039330507,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">AMPA</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">A AMPA-MT (Associação Mato-grossense dos Produtores de Algodão) é uma das mais importantes e influentes entidades do agronegócio brasileiro, representando os cotonicultores do estado de Mato Grosso, o maior produtor de algodão do país. Fundada em 1997, a associação tem como missão principal defender os interesses dos seus associados, promover a sustentabilidade da cotonicultura e fortalecer a competitividade da fibra mato-grossense nos mercados nacional e internacional.</font></font>"
+        }
+      ]
+    }
+  ],
+  "name": "Centro Politico Administrativo",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
